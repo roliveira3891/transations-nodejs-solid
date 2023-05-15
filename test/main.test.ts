5fb40dc8e2e8488b3a74ff3e2ba76fa97ae07fe4
Renato <roliveira3891@gmail.com>
2023-05-14 23:23:02 -0300

    Primeiro

---
+++ test/main.test.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+test("Deve Criar uma transação", async function(){
+    const code = `${Math.floor(Math.random()*1000)}`
+    await axios({
+        url: "http://localhost:3000/transactions",
+        method: "post",
+        data: {
+            code,
+            amount: 1000,
+            numberInstallments: 12,
+            paymentMethod: "credit_card"
+      
+        }
+    })
+
+
+    const response = await axios({
+        url: `http://localhost:3000/transactions/${code}`,
+        method: "get"
+    })
+
+    const transaction = response.data
+
+    // console.log('->', transaction)
+   
+    expect(transaction.code).toBe(code)
+    expect(transaction.amount).toBe(1000)
+    expect(transaction.payment_method).toBe("credit_card")
+    expect(transaction.installments).toHaveLength(12)
+    expect(transaction.installments[0].amount).toBe(83.33)
+    expect(transaction.installments[11].amount).toBe(83.37)
+})
